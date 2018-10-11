39ec5023c3982e759ac6b48af3f7d063875549cc
rkemmy <rkemmy.rk69@gmail.com>
2018-10-11 14:43:15 +0300

    [#160812156] Create a function that allows user to login

---
+++ UI/assets/js/login.js
@@ -0,0 +1,45 @@
+var login = document.getElementById('login')
+
+login.onclick = function(){
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+
+    fetch('http://127.0.0.1:5000/api/v2/auth/login',{
+        method:'POST',
+        mode:'cors',
+        headers:{
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json'  
+        },
+        body: JSON.stringify({
+            "username":username,
+            "password":password})
+            
+    })
+
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        console.log(data['message'])
+        let leon = 'You were successfully logged in ' + username;
+        console.log(leon)
+        if (data['message'] === leon){
+            document.getElementById('username').value = "";
+            document.getElementById('password').value = "";
+            window.localStorage.setItem('token', data['token'])
+            redirect: window.location.replace("../html/index.html")
+        
+            if (data["admin"] === true){
+                redirect: window.location.replace("../html/admin/portal.html")
+            }
+            else{
+                redirect: window.location.replace("../html/index.html")
+            }
+        }
+
+        else{
+            document.getElementById('loginfailed').innerHTML = "Invalid email address or password";
+            document.getElementById('loginfailed').style.color = "red";
+        }
+    })
+}
